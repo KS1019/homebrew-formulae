ad0bb35ced6c623e367164b253d70e040e90f580
KOTARO SUTO <ng1a5ss43@gmail.com>
2021-07-25 18:00:04 +0900

    Add swprofiler.rb

---
+++ swprofiler.rb
@@ -0,0 +1,17 @@
+class Swprofiler < Formula
+  desc "Simple profiler for compilation time of your Swift project, written in Swift."
+  homepage "https://github.com/KS1019/SwiftyProfiler"
+  url "https://github.com/KS1019/SwiftyProfiler/archive/refs/tags/0.0.3.tar.gz"
+  sha256 "c18aa185ed7c7e18c4499aa213781fca75e7a6923b596f35ab26c38009e90600"
+  license ""
+
+  depends_on :xcode => ["10.0", :build]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
