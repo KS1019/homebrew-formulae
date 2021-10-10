3efbae817a36b5dc807a65eacd941eac3b710ce0
Kotaro Suto <ng1a5ss43@gmail.com>
2021-10-10 14:28:22 -0700

    Update SwiftyProfiler to 0.0.8

--- swprofiler.rb
+++ swprofiler.rb
@@ -1,7 +1,7 @@
 class Swprofiler < Formula
   desc "Simple profiler for compilation time of your Swift project, written in Swift."
   homepage "https://github.com/KS1019/SwiftyProfiler"
-  url "https://github.com/KS1019/SwiftyProfiler.git", tag: "refs/heads/main", revision: "c84b39f72041c6689e95a608d3b63c15545d1d9e"
+  url "https://github.com/KS1019/SwiftyProfiler.git", tag: "0.0.8", revision: "43d9d0819c7ffb83226c8b19979309b67ef94a9e"
 
   depends_on :xcode => ["10.0", :build]
 
