32c366c6e1eb1449f06086c58683c1b60c884cb1
Kotaro Suto <ng1a5ss43@gmail.com>
2021-12-28 00:25:56 -0800

    Fix syntax

--- hondana.rb
+++ hondana.rb
@@ -2,8 +2,8 @@ class Hondana < Formula
     desc "CLI tool to manage your bookmarklets"
     homepage "https://github.com/KS1019/Hondana"
     url "https://github.com/KS1019/Hondana.git"
-    tag: "0.0.1"
-    revision: "05f0a7df21190740fdccc09dffad621bce8015e0"
+    tag "0.0.1"
+    revision "05f0a7df21190740fdccc09dffad621bce8015e0"
   
     depends_on :xcode => ["10.0", :build]
   
