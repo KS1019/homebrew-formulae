d1d9361e4c5979c1cea862d1ba7afa1838ed303c
Kotaro Suto <ng1a5ss43@gmail.com>
2021-12-28 00:35:24 -0800

    Add license

--- hondana.rb
+++ hondana.rb
@@ -3,6 +3,8 @@ class Hondana < Formula
     homepage "https://github.com/KS1019/Hondana"
     url "https://github.com/KS1019/Hondana.git", tag: "0.0.1", revision: "05f0a7df21190740fdccc09dffad621bce8015e0"
   
+    license "MIT"
+
     depends_on :xcode => ["10.0", :build]
   
     def install
