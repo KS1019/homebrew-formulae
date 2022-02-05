40104150d36e932807090a3287ee045d8e3e6c0c
Kotaro Suto <ng1a5ss43@gmail.com>
2022-02-05 00:27:21 -0800

    Add new fields in hondana.rb

--- hondana.rb
+++ hondana.rb
@@ -2,7 +2,10 @@ class Hondana < Formula
     desc "CLI tool to manage your bookmarklets"
     homepage "https://github.com/KS1019/Hondana"
     url "https://github.com/KS1019/Hondana/archive/0.0.3.tar.gz"
-  
+    sha256 ""
+    tag ""
+    revision ""
+    
     license "MIT"
 
     depends_on :xcode => ["10.0", :build]
