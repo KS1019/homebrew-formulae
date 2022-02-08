fb91b5cf949d607028f6e27c7b70cfd618bb45b7
Kotaro Suto <ng1a5ss43@gmail.com>
2022-02-07 17:44:00 -0800

    Update hondana.rb

--- hondana.rb
+++ hondana.rb
@@ -1,9 +1,8 @@
 class Hondana < Formula
     desc "CLI tool to manage your bookmarklets"
     homepage "https://github.com/KS1019/Hondana"
-    url "https://github.com/KS1019/Hondana.git"
+    url "https://github.com/KS1019/Hondana/archive/0.0.5.tar.gz"
     
-    sha256 "OLDSHA"
     license "MIT"
 
     depends_on :xcode => ["10.0", :build]
