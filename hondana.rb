2441d01c166d1a24b413d0d2cb6d138147e5878f
Kotaro Suto <ng1a5ss43@gmail.com>
2022-02-07 17:41:26 -0800

    hondana 0.0.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- hondana.rb
+++ hondana.rb
@@ -1,7 +1,7 @@
 class Hondana < Formula
     desc "CLI tool to manage your bookmarklets"
     homepage "https://github.com/KS1019/Hondana"
-    url "https://github.com/KS1019/Hondana/archive/0.0.5.tar.gz"
+    url "https://github.com/KS1019/Hondana.git"
     
     sha256 "OLDSHA"
     license "MIT"
