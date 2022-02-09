10f38aa8c46e379216e5867917715440a2a1ad27
Kotaro Suto <ng1a5ss43@gmail.com>
2022-02-08 16:56:48 -0800

    Update hondana.rb

--- hondana.rb
+++ hondana.rb
@@ -2,6 +2,7 @@ class Hondana < Formula
     desc "CLI tool to manage your bookmarklets"
     homepage "https://github.com/KS1019/Hondana"
     url "https://github.com/KS1019/Hondana/archive/0.0.6.tar.gz"
+    sha256 "3265498428ccbc8535fa22fd5f6cab65f44e790cf9677294fe0a1c28a8e9931e"
     
     license "MIT"
 
