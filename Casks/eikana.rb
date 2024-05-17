253afa7e6af3758969dfffbdea8a96bbded5b004
Kotaro Suto <ng1a5ss43@gmail.com>
2024-05-17 16:46:52 -0700

    Update eikana.rb

--- Casks/eikana.rb
+++ Casks/eikana.rb
@@ -5,6 +5,8 @@ cask "eikana" do
   desc "Input Mode Switcher for Japanese and English / USキーボードで日英入力切り替えを行うアプリ"
   homepage "https://github.com/KS1019/eikana"
   sha256 "03e15c75b1d71a265b9c063fa0e6d3327d7a919ce922e8e4ae167788ebd3d1ba"
+
+  hasCodeSignTool = system "which codesign > /dev/null 2>&1"
   
   
   livecheck do
@@ -13,10 +15,20 @@ cask "eikana" do
   end
   
   caveats do
-    unsigned_accessibility
+    if hasCodeSignTool
+      caveats "Self signing #{name}.app with codesign tool"
+    else
+      unsigned_accessibility
+    end
   end
   
   depends_on macos: ">= :sonoma"
   
   app "eikana.app"
+
+  def post_install
+    if hasCodeSignTool
+      system "codesign --force --deep -s - /Applications/#{name}.app"
+    end
+  end
 end
