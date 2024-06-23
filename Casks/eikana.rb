6770c70314485dc62a4004f82ac4fb7e6e529280
Kotaro Suto <ng1a5ss43@gmail.com>
2024-06-22 17:05:28 -0700

    Update eikana.rb

--- Casks/eikana.rb
+++ Casks/eikana.rb
@@ -5,28 +5,19 @@ cask "eikana" do
   desc "Input Mode Switcher for Japanese and English / USキーボードで日英入力切り替えを行うアプリ"
   homepage "https://github.com/KS1019/eikana"
   sha256 "ff891db2e298a7bc944bc37be3608d2a4adf44072031e7f4aeeef13a6a54f56e"
-
-  hasCodeSignTool = system "which codesign > /dev/null 2>&1"
-  
   
   livecheck do
     url :url
     strategy :github_latest
   end
-  
-  if not hasCodeSignTool
-    caveats do
-      unsigned_accessibility
-    end
-  end
+
+  caveats "#{name} is not notarized and will delete com.apple.quarantine attribute automatically during the installation."
   
   depends_on macos: ">= :sonoma"
   
   app "eikana.app"
 
-  def post_install
-    if hasCodeSignTool
-      system "codesign --force --deep -s - /Applications/#{name}.app"
-    end
+  def post_flight
+    system "xattr -d com.apple.quarantine #{appdir}/eikana.app"
   end
 end
