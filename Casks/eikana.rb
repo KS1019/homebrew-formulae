c25559689664b391b59e1279f9cd8578a4d6394a
Kotaro Suto <ng1a5ss43@gmail.com>
2024-06-23 16:31:27 -0700

    Update eikana.rb

--- Casks/eikana.rb
+++ Casks/eikana.rb
@@ -1,10 +1,10 @@
 cask "eikana" do
-  version "0.0.2"
+  version "0.0.3"
   url "https://github.com/KS1019/eikana/releases/download/#{version}/eikana.zip"
   name "eikana"
   desc "Input Mode Switcher for Japanese and English / USキーボードで日英入力切り替えを行うアプリ"
   homepage "https://github.com/KS1019/eikana"
-  sha256 "ff891db2e298a7bc944bc37be3608d2a4adf44072031e7f4aeeef13a6a54f56e"
+  sha256 "2ec13beeaa4c98537a49c114cb006075d6f0ce8bf596619082db3e9f610b0299"
   
   livecheck do
     url :url
